aaf8a649f7c5ece781c66c5df42ade289f2e46c1
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-27 20:29:15 -0500

    create roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,13 @@ VALUES
 ('Accounting'),
 ('IT'),
 ('Customer Service');
+
+INSERT INTO roles (job_title, dept_id, salary)
+VALUES
+('Manager', 6, 100000),
+('Project Manager', 2, 75000),
+('Graphic Designer', 3, 50000),
+('Bookkeeper', 6, 45000),
+('Phone Agent', 4, 35000),
+('Senior Developer', 5, 80000),
+('Assitant Manager', 1, 75000);
--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // import function
 const { promptUser } = require('./lib/promptUser');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '${SunFlower29}',
+    database: 'employee_tracker'
+  },
+  console.log('Connected to the employee_tracker database')
+)
+
+// db.query(`SELECT * FROM departments`, (err, rows) => {
+//   console.log(rows)
+// });
 
 // inititalize app
-promptUser();
+// promptUser();
